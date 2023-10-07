bd4d81d7d941156fd724aa022aa8a2b6eca65ab2
Nikita Kirilloff PC <Enrage1437@gmail.com>
2023-10-07 23:13:57 +0500

    1 task commit

--- 2020-08/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/Main.java
+++ 2020-08/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/Main.java
@@ -9,9 +9,9 @@ public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("/spring-context.xml");
-        // TODO: создайте здесь класс контекста
+
         PersonService service = context.getBean(PersonService.class);
-        // TODO: Получите Person Service
+
         // Получите Person "Ivan"
         Person ivan = service.getByName("Ivan");
         System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
