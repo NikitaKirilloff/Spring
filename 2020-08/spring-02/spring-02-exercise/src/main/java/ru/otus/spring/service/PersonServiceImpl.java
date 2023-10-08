70c759f1d75958c38758651769e87d029fa50e12
Nikita Kirilloff Notebook <Enrage1437@gmail.com>
2023-10-09 01:33:47 +0500

    2 task

--- 2020-08/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
+++ 2020-08/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
@@ -1,13 +1,23 @@
 package ru.otus.spring.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
 import ru.otus.spring.dao.PersonDao;
 import ru.otus.spring.domain.Person;
 
+
+@Service
+@Configuration
+@Primary
 public class PersonServiceImpl implements PersonService {
 
     private final PersonDao dao;
 
-    public PersonServiceImpl(PersonDao dao) {
+
+    public PersonServiceImpl(@Qualifier("personDao") PersonDao dao) {
         this.dao = dao;
     }
 
