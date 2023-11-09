d608eb672d8151d7268e5bf9861cd0c78d708572
Nikita Kirilloff Notebook <Enrage1437@gmail.com>
2023-11-09 22:34:03 +0500

    `2 task`

--- 2020-08/spring-05-adv-conf/beans-scopes-exercise/src/main/java/ru/otus/example/beansscopesdemo/services/RequestGreetingServiceImpl.java
+++ 2020-08/spring-05-adv-conf/beans-scopes-exercise/src/main/java/ru/otus/example/beansscopesdemo/services/RequestGreetingServiceImpl.java
@@ -1,7 +1,10 @@
 package ru.otus.example.beansscopesdemo.services;
 
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
 
+@RequestScope(proxyMode = ScopedProxyMode.INTERFACES)
 @Service("RequestGreetingService")
 public class RequestGreetingServiceImpl extends AbstractGreetingServiceImpl {
 }
--- 2020-08/spring-05-adv-conf/beans-scopes-exercise/src/main/java/ru/otus/example/beansscopesdemo/services/SessionGreetingServiceImpl.java
+++ 2020-08/spring-05-adv-conf/beans-scopes-exercise/src/main/java/ru/otus/example/beansscopesdemo/services/SessionGreetingServiceImpl.java
@@ -1,7 +1,10 @@
 package ru.otus.example.beansscopesdemo.services;
 
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
 
+@SessionScope(proxyMode = ScopedProxyMode.INTERFACES)
 @Service("SessionGreetingService")
 public class SessionGreetingServiceImpl extends AbstractGreetingServiceImpl {
 }
