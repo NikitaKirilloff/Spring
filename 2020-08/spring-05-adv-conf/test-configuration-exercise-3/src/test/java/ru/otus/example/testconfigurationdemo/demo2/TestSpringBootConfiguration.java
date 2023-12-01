831f3c32e6df1898ecdea9cd52f326aa37af86a5
Nikita Kirilloff Notebook <Enrage1437@gmail.com>
2023-12-02 01:20:40 +0500

    02.12

--- 2020-08/spring-05-adv-conf/test-configuration-exercise-3/src/test/java/ru/otus/example/testconfigurationdemo/demo2/TestSpringBootConfiguration.java
+++ 2020-08/spring-05-adv-conf/test-configuration-exercise-3/src/test/java/ru/otus/example/testconfigurationdemo/demo2/TestSpringBootConfiguration.java
@@ -2,23 +2,27 @@ package ru.otus.example.testconfigurationdemo.demo2;
 
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import ru.otus.example.testconfigurationdemo.family.FamilyMember;
-import ru.otus.example.testconfigurationdemo.family.childrens.Son;
 import ru.otus.example.testconfigurationdemo.family.parents.Father;
-import ru.otus.example.testconfigurationdemo.family.parents.Mother;
+import ru.otus.example.testconfigurationdemo.family.pets.Dog;
 
+
+@ComponentScan(value = "ru.otus.example.testconfigurationdemo.family",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = Dog.class))
 @SpringBootConfiguration
 public class TestSpringBootConfiguration {
     @Bean
     public FamilyMember father() {
         return new Father();
     }
-    @Bean
+   /* @Bean
     public FamilyMember mother() {
         return new Mother();
     }
     @Bean
     public FamilyMember son() {
         return new Son();
-    }
+    }*/
 }
