f3fb537cf0b640b95b699c9bc99e1cb4ffacac07
Nikita Kirilloff Notebook <Enrage1437@gmail.com>
2023-12-02 01:13:24 +0500

    02.12

--- 2020-08/spring-05-adv-conf/test-configuration-exercise-3/src/test/java/ru/otus/example/testconfigurationdemo/demo2/SpringBootTestWithExternalLimitationDemoTest.java
+++ 2020-08/spring-05-adv-conf/test-configuration-exercise-3/src/test/java/ru/otus/example/testconfigurationdemo/demo2/SpringBootTestWithExternalLimitationDemoTest.java
@@ -4,7 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import ru.otus.example.testconfigurationdemo.family.FamilyMember;
+import ru.otus.example.testconfigurationdemo.family.childrens.Son;
+import ru.otus.example.testconfigurationdemo.family.parents.Father;
+import ru.otus.example.testconfigurationdemo.family.parents.Mother;
 
 import java.util.Map;
 
@@ -13,6 +18,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("В SpringBootTestWithExternalLimitationDemoTest семья должна ")
 @SpringBootTest
 public class SpringBootTestWithExternalLimitationDemoTest {
+
+    /*@TestConfiguration
+    static class SpringBootTestWithExternalLimitationDemo {
+        @Bean
+        public FamilyMember father() {
+            return new Father();
+        }
+        @Bean
+        public FamilyMember mother() {
+            return new Mother();
+        }
+        @Bean
+        public FamilyMember son() {
+            return new Son();
+        }
+    }*/
 
     @Autowired
     private Map<String, FamilyMember> family;
--- 2020-08/spring-05-adv-conf/test-configuration-exercise-3/src/test/java/ru/otus/example/testconfigurationdemo/demo2/TestSpringBootConfiguration.java
+++ 2020-08/spring-05-adv-conf/test-configuration-exercise-3/src/test/java/ru/otus/example/testconfigurationdemo/demo2/TestSpringBootConfiguration.java
@@ -1,4 +1,24 @@
 package ru.otus.example.testconfigurationdemo.demo2;
 
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import ru.otus.example.testconfigurationdemo.family.FamilyMember;
+import ru.otus.example.testconfigurationdemo.family.childrens.Son;
+import ru.otus.example.testconfigurationdemo.family.parents.Father;
+import ru.otus.example.testconfigurationdemo.family.parents.Mother;
+
+@SpringBootConfiguration
 public class TestSpringBootConfiguration {
+    @Bean
+    public FamilyMember father() {
+        return new Father();
+    }
+    @Bean
+    public FamilyMember mother() {
+        return new Mother();
+    }
+    @Bean
+    public FamilyMember son() {
+        return new Son();
+    }
 }
